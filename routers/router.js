48e79d874a6421f6872bcfbc8d4ec9f76f36434f
Paweł Rostek <pawel.rostek@i-bs.pl>
2015-04-09 15:23:18 +0200

    Geting data from google doc

--- routers/router.js
+++ routers/router.js
@@ -1,34 +1,27 @@
 
 var routerApp = angular.module('routerApp', 
 	[
+  'Tabletop',
 	'ngResource', 
 	'ngRoute',
-    'ngCookies'
+  'ngCookies',
+  'Gdoc',
+  'pageService'
 	]);
 
 routerApp
-
-// .config(function($httpProvider) {
-//         $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-//         $httpProvider.defaults.useXDomain = true;
-//         $httpProvider.defaults.withCredentials = true;
-//         delete $httpProvider.defaults.headers.common["X-Requested-With"];
-//         $httpProvider.defaults.headers.common["Accept"] = "application/json";
-//         $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-//     })
-
-.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+  .config(['$sceDelegateProvider', function($sceDelegateProvider) {
          $sceDelegateProvider.resourceUrlWhitelist(['self', 'https://script.googleusercontent.com**', 'http://drive.google.com/**', 'https://docs.google.com/**']);
      }])
 
-.config(function($routeProvider) {
-    $routeProvider
-    .when('/',
-    {
-        controller: 'mainController',
-        templateUrl: 'partial/main.html'
-    })
-    .otherwise({
-        redirectTo: '/'
-    })
-  });
+  .config(function($routeProvider) {
+      $routeProvider
+      .when('/',
+      {
+          controller: 'mainController',
+          templateUrl: 'partial/main.html'
+      })
+      .otherwise({
+          redirectTo: '/'
+      })
+    });
--- services/page.js
+++ services/page.js
@@ -3,6 +3,15 @@ angular.module('pageService', [])
     return {
         setTitle: function(title){
             $rootScope.title = title;
+        },
+       setSiteName: function(site_name){
+            $rootScope.site_name = site_name;
+        },
+       setSiteDescription: function(site_description){
+            $rootScope.site_description = site_description;
+        },
+       loadedPage: function(value){
+            $rootScope.loaded_page = value;
         }
     }
 })
