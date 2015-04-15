89b4fc2b35262dd9cd9a858fa9cdbb3255e8f689
Paweł Rostek <pawel.rostek@i-bs.pl>
2015-04-15 15:11:01 +0200

    Add notify 1

--- controllers/startController.js
+++ controllers/startController.js
@@ -7,7 +7,7 @@
 angular.module('routerApp')
 	.controller('startController', startController);
 
-function startController($scope, $routeParams, $http, $timeout, wdGdoc, $modal, wdPageService) {
+function startController($scope, $routeParams, $http, $timeout, wdGdoc, $modal, wdPageService, notify) {
 
   wdGdoc.query(function(data) {
       $scope.spreadsheet = data;
@@ -80,7 +80,25 @@ function startController($scope, $routeParams, $http, $timeout, wdGdoc, $modal, 
   };
 
 
+  // Notify
+ $scope.myNotify = function(message){
 
+   $scope.msg = 'Hello! This is a sample message!';
+   $scope.template = '';
+
+   $scope.positions = ['right', 'center', 'left'];
+   $scope.position = $scope.positions[0];
+
+   $scope.duration = 3000;
+
+   notify({
+           message: message,
+           classes: $scope.classes,
+           templateUrl: $scope.template,
+           position: $scope.position,
+           duration: $scope.duration
+       });
+ };
 
 
 
